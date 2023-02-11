c36b666ed01343dd2a6ee16e036439afca15b977
RrLSn <afolabisodiqtemitope@yahoo.com>
2023-02-11 22:57:25 +0100

    Tasks 1.1-1.2

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,44 +1,54 @@
-import './App.css';
-
-const Hello = (props) => {
-  console.log(props)
-  return (
+const Header = (props) => {
+  return(
     <div>
-      <p>Hello {props.name}, you're {props.age} years old</p>
+      <p>
+        {props.course}
+      </p>
     </div>
   )
 }
 
-const SayingHello = () => {
+const Content = (props) => {
   return(
-  <div>
-    <p>How're you doing </p>
-  </div>
+    <div>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+    </div>
   )
 }
 
-const Footer = () => {
-  return (
+const Total = (props) => {
+  return(
     <div>
-      greeting app created by 
-      <a href="https://github.com/mluukkai">mluukkai</a>
+      <p>
+        Number of Exervice is {props.numExercise}
+      </p>
     </div>
   )
 }
 
 const App = () => {
-  const nimi = 'Pekka'
-  const ika = 10
-  const friends = [ 'Leevi', 'Venla']
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
   return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name= "Maya" age= {26+10}/>
-      <Hello name= {nimi} age= {ika}/>
-      <p>{friends}</p>
-      <SayingHello />
-      <Footer />
-    </>
+    <div>
+      <Header course= {course}/>
+      <Content part1= {part1} exercises1= {exercises1} part2= {part2} exercises2= {exercises2} part3= {part3} exercises3= {exercises3}/>
+      <Total numExercise= {exercises1 + exercises2 + exercises3}/>
+    </div>
   )
 }
 
