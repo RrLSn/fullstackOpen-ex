d2e52e5424a990d520af45e4baf71eec9f4d4f57
RrLSn <afolabisodiqtemitope@yahoo.com>
2023-02-15 21:02:58 +0100

    course information, step2

---
+++ part1/courseInfo/src/Components/Total.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Total = (props) => {
+    return(
+        <div>
+          <p>
+            Number of Exervice is {props.numExercise}
+          </p>
+        </div>
+      )
+}
+
+export default Total
